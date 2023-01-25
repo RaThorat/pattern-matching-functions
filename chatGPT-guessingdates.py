69a72602a3c947078855b961ed99ce0642e59f9a
Rahul Thorat <61758268+RaThorat@users.noreply.github.com>
2023-01-25 04:26:41 +0100

    Create chatGPT-guessingdates.py

---
+++ chatGPT-guessingdates.py
@@ -0,0 +1,20 @@
+#improving on original function
+import dateutil.parser
+
+def guess_date(string):
+    date = dateutil.parser.parse(string, fuzzy=True)
+    return date.date()
+
+#This code uses the dateutil library's parse function, which is more robust and can handle a wider variety of date formats. 
+#The fuzzy=True option allows for more lenient parsing of the input string, which makes it more likely to find a valid date.
+
+#You can also add a try-except block to handle cases when no valid date can be found in the input string.
+import dateutil.parser
+
+def guess_date(string):
+    try:
+        date = dateutil.parser.parse(string, fuzzy=True)
+        return date.date()
+    except ValueError:
+        return None
+#This way, you can check if the returned value is None or not, if yes then you can raise ValueError(string) or handle it in any other way you want.
